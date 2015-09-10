bc917960561d12186470997277efb451690cb074
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-10 13:22:12 -0700

    Spec the flip method

--- spec/flip_book_spec.rb
+++ spec/flip_book_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
+require 'flip_book'
 
 describe FlipBook do
-  it "has a reverse method" do
+  before do
+    content = "road! brick yellow the Follow"
+    @book = FlipBook.new(content)
+  end
+
+  it "has a flip method" do
+    @book.flip.must_equal "Follow the yellow brick road!"
   end
 end
